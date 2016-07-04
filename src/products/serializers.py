12c8507f972832b307186587b30780313a7a0d1f
hshafy <hshafy@users.noreply.github.com>
2016-07-04 21:07:35 +0200

    productgroup can't created without at least one product associated with it's basic data

--- src/products/serializers.py
+++ src/products/serializers.py
@@ -11,6 +11,11 @@ class ProductPropertySerializer(HyperlinkedModelSerializer):
         #fields = ('url', 'key', 'value')
 
 class ProductSerializer(HyperlinkedModelSerializer):
+	"""
+	Product Serializer with nested properties.
+
+	Product can be created without properites, then properties can be assigned later.
+	"""
 	properties = ProductPropertySerializer(
 		many=True,
 		read_only=True,
@@ -20,11 +25,22 @@ class ProductSerializer(HyperlinkedModelSerializer):
 		#fields = ('url', 'product_variation', 'goss_price', 'tax', 'stock', 'properties')
 
 class ProductGroupSerializer(HyperlinkedModelSerializer):
+	"""
+	Product Group Serializer with nested products.
+
+	ProductGroup can't be created without the nested products.
+	"""
 	products = ProductSerializer(
 		many=True,
-		read_only=True,
       	)
 	class Meta:
 		model = ProductGroup
 		depth = 1
 		fields = ('url', 'name', 'products')
+
+	def create(self, validated_data):
+		products_data = validated_data.pop('products')
+		product_group = ProductGroup.objects.create(**validated_data)
+		for product_data in products_data:
+			Product.objects.create(product_group=product_group, **product_data)
+		return product_group
--- src/products/tests.py
+++ src/products/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from products.models import ProductGroup
+
+class ProductGroupTests(APITestCase):
+    def test_cant_create_productgroup_without_basic_data(self):
+        """
+        Ensure we can't create a product group without the basic data (at least with one related product)
+        """
+        url = reverse('productgroup-list')
+        data = {'name': 'test product group'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(ProductGroup.objects.count(), 0)
